2434f97b310bd18c76f0e703849f8c86839bd190
Justin Ferraz <ailton.ferrazjr@gmail.com>
2024-12-22 15:56:16 -0300

    feature: class QuestionFetcher with the graphql query ready to fetch from Leetcode API

---
+++ src/utils/questionFetcher/query.ts
@@ -0,0 +1,27 @@
+const questionContentQuery = `query questionContent($titleSlug: String!) {
+    question(titleSlug: $titleSlug) {
+      content
+      mysqlSchemas
+    }
+  }`;
+
+
+
+const questionDataQuery = `query questionTitle($titleSlug: String!) {
+  question(titleSlug: $titleSlug) {
+    questionId
+    questionFrontendId
+    title
+    titleSlug
+    isPaidOnly
+    difficulty
+    likes
+    dislikes
+  }
+}
+`
+
+export const queries = {
+    questionContentQuery,
+    questionDataQuery
+};
