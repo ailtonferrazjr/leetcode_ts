de16353f22eb5c833be87df690f0f537549777e2
Justin Ferraz <ailton.ferrazjr@gmail.com>
2024-12-24 14:29:53 -0300

    solved: 2621 with tests

---
+++ src/easy/2621_sleep.ts
@@ -0,0 +1,18 @@
+/*
+ * 2621 | Sleep
+ * Difficulty: Easy
+ * ----------------
+ *
+ * Description:
+ * Given a positive integer "millis", write an asynchronous function that sleeps
+ * for "millis" milliseconds. It can resolve any value.
+ *
+ * URL: https://leetcode.com/problems/sleep/
+*/
+
+export async function sleep(millis: number): Promise<void> {
+    return new Promise<void>(resolve => {
+     setTimeout(resolve, millis);
+     })
+}
+
