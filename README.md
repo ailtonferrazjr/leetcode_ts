1451eeada749ed4b78866c17b60e3f9565a4a147
Justin Ferraz <ailton.ferrazjr@gmail.com>
2024-12-24 12:06:34 -0300

    solved: 2666 question solved and tested

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This repository contains my solutions to various LeetCode problems implemented i
 Each solution includes detailed explanations, time and space complexity analysis, and test cases.
 
 ## Summary of Solutions
-- Easy: 9
+- Easy: 10
 - Medium: 0
 - Hard: 0
 
---
+++ src/easy/2666_allow_one_function_call.ts
@@ -0,0 +1,28 @@
+/*
+ * 2666 | Allow One Function Call
+ * Difficulty: Easy
+ * ----------------
+ *
+ * Description:
+ * Given a function "fn", return a new function that is identical to the
+ * original function except that it ensures "fn" is called at most once.
+ * 
+ * -> The first time the returned function is called, it should return the same
+ * result as fn.
+ * -> Every subsequent time it is called, it should return undefined.
+ *
+ * URL: https://leetcode.com/problems/allow-one-function-call/
+*/
+
+export type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+export type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+export function once(fn: Function): OnceFn {
+    let hasBeenCalled: boolean = false;
+    return function (...args) {
+        if (hasBeenCalled) return undefined;
+        hasBeenCalled = true;
+        return fn(...args)
+        
+    };
+}
